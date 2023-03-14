4a980a3ac03d7c455ec62b060024cd30faad0eb4
ViatkinDanila <epam0921@gmail.com>
2023-03-14 13:40:57 +0400

    Init commit

---
+++ src/test/java/io/waves/WavesCryptoExchangerBot/WavesCryptoExchangerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.waves.WavesCryptoExchangerBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavesCryptoExchangerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
